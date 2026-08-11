d404f2779b8fac92d99204468408277434403ce4
pkumar1618 <prashanthch943@gmail.com>
2026-08-11 12:17:45 +0530

    Terraform VPC module

--- output.tf
+++ output.tf
@@ -5,6 +5,27 @@ output "vpc_id" {
   # depends_on = []
 }
 
-output "az_info" {
-  value = data.aws_availability_zones.available
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet_database[*].id
+}
+
+# output "az_info" {
+#   value = data.aws_availability_zones.available
+# }
+
+# output "default_vpc_info" {
+#   value = data.aws_vpc.default
+# }
+
+# output "main_route_table_info" {
+#   value = data.aws_route_table.main
+
+# }
